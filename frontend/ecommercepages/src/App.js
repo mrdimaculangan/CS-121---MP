688dfa509547a8a1fc35ec77ca8a8e3126a0bbcb
mariaamor <mgeco@up.edu.ph>
2025-04-30 23:30:14 +0800

    started profile information page

--- frontend/ecommercepages/src/App.js
+++ frontend/ecommercepages/src/App.js
@@ -1,10 +1,14 @@
 import ItemDescription from "./components/pages/itemdescription";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './components/assets/Bayer\ TypeArchiType\ Regular.otf';
+import ProfileInformation from "./components/pages/profileinformation";
 
 
 function App() {
   return (
-    <ItemDescription />
+    <>
+    <ProfileInformation />
+    </>
   );
 }
 
--- frontend/ecommercepages/src/components/pages/profileinformation.jsx
+++ frontend/ecommercepages/src/components/pages/profileinformation.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './itemdescription.css';
+import './profileinformation.css';
 import Cart from '../assets/cart.png';
 import Heart from '../assets/heart.png';
 import Home from '../assets/home.png';
 import Profile from '../assets/profile.png';
 import Search from '../assets/search.png';
-import Frame from '../assets/frame.png';
-import ProfileInformation from './profileinformation';
+import Transparent from '../assets/transparent.png';
+import Flower from '../assets/flower.png'
 
-const ItemDescription = () => {
+const ProfileInformation = () => {
   return (
-    <div className="page-container">
+    <div className="profilepage-container">
     <nav className="navbar">
     <div className="navbar-container">
       <ul className="navbar-links-left">
@@ -22,28 +22,31 @@ const ItemDescription = () => {
         <button class="button" type="submit"><img src={Search} alt="Description" style={{ width: '25px' }} /></button>
       </form>
       <ul className="navbar-links-right">
-        <li><a href={ProfileInformation}><img src={Profile} alt="Description" style={{ width: '40px' }} /></a></li>
+        <li><a href="/"><img src={Profile} alt="Description" style={{ width: '40px' }} /></a></li>
         <li><a href="/"><img src={Heart} alt="Description" style={{ width: '40px' }} /></a></li>
         <li><a href="/"><img src={Cart} alt="Description" style={{ width: '36px' }} /></a></li>
     </ul>
     </div>
   </nav>
-  <div className="content">
-    <div className="productimage">
-      <img src={Frame} alt="frame" style={{width: '570px'}} className="frame"/>
-    </div>
-    <div className="productdescription">
-      <h1 style={{fontFamily: 'Bayer TypeArchiType Regular' }} className='productname'>Product Name</h1>
-      <h3 style={{fontFamily: 'Times New Roman' }} className='price'>P0.00</h3>
-      <h2 style={{fontFamily: 'Bayer TypeArchiType Regular' }} className='description'>Short Product Description</h2>
-      <div className='buttongroup'>
-      <button class="button" type="submit" className='fav'><img src={Heart} alt="Fav"/></button>
-      <button class="button" type="submit" className='add'>add to bag</button>
+  <div className="profile-info">
+    <div className="container">
+      <div className="main">
+        <div className="content">
+        <img src={Flower} alt="flower" style={{width: '90px'}} className='flowerpic'/>
+            <h1>welcome, user!</h1>
+            <h2>here are your information:</h2>
+            <h3 className='email'>email: name@email.com</h3>
+            <h3 className='username'>username: username</h3>
+            <button class="button" type="submit" className='logout'>log out</button>
+      </div>
+      <div className="pic">
+                  <img id = "photo" src={Transparent} alt="nature"></img>
+        </div>
       </div>
     </div>
   </div>
   </div>
-  )
-}
+  );
+};
 
-export default ItemDescription;
+export default ProfileInformation;
