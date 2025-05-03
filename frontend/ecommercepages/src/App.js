5dd6704ca3bf721222d0a82ea60a6f65ee939c83
mrdimaculangan <mrdimaculangan@up.edu.ph>
2025-05-03 20:14:48 +0800

    temporarily restored old version

--- frontend/ecommercepages/src/App.js
+++ frontend/ecommercepages/src/App.js
@@ -2,12 +2,12 @@ import ItemDescription from "./components/pages/itemdescription";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './components/assets/Bayer\ TypeArchiType\ Regular.otf';
 import ProfileInformation from "./components/pages/profileinformation";
-import LoginRegister from "./components/pages/login";
+
 
 function App() {
   return (
     <>
-    <LoginRegister /> 
+    <ProfileInformation /> 
   
     </>
   );
