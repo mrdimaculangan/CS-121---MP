adbff8aa40b0f6c598d709b8d9612292bceaf647
mrdimaculangan <mrdimaculangan@up.edu.ph>
2025-05-18 00:44:06 +0800

    connected django to react
    products are dynamically added from django (seller) then it automatically updates the items in shop then itemdescription too

--- frontend/ecommercepages/src/components/pages/buyer/shop.jsx
+++ frontend/ecommercepages/src/components/pages/buyer/shop.jsx
@@ -1,44 +1,39 @@
-import React from 'react'
-import Navbar from './navbar'
-import styles from './shop.module.css'
-import flower from '../../assets/flower.jpeg'
-import amor from '../../assets/amor.png';
-import bianca from '../../assets/bianca.png';
-import mari from '../../assets/mari.png';
-import ItemDescription from "../../pages/buyer/itemdescription";
-import shop from '../../pages/buyer/shop'
+import React, { useEffect, useState } from 'react';
+import Navbar from './navbar';
+import styles from './shop.module.css';
 import { Link } from 'react-router-dom';
 
-
 const Shop = () => {
-    return (
-     <div className={styles.shopContainer}>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/products/')
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error("Failed to load products:", err));
+  }, []);
+
+  return (
+    <div className={styles.shopContainer}>
       <Navbar />
       <div className={styles.shopSection}>
         <h1>SHOP SECTION</h1>
         <div className={styles.productGrid}>
-            <Link to="/itemdescription" className={styles.productCard}>
-                <img src={amor} alt="Amorous Amor" />
-                <p className={styles.productName}>Amorous Amor</p>
-                <p className={styles.productPrice}>₱99,999</p>
+          {products.map((product) => (
+            <Link
+              key={product.id}
+              to={`/itemdescription/${product.id}`}
+              className={styles.productCard}
+            >
+              <img src={product.image} alt={product.name} />
+              <p className={styles.productName}>{product.name}</p>
+              <p className={styles.productPrice}>₱{product.price}</p>
             </Link>
-
-            <Link to="/itemdescription" className={styles.productCard}>
-                <img src={bianca} alt="Bianca's Baby" />
-                <p className={styles.productName}>Bianca's Baby</p>
-                <p className={styles.productPrice}>₱89,990</p>
-            </Link>
-
-            <Link to="/itemdescription" className={styles.productCard}>
-                <img src={mari} alt="Marry Me, Mari" />
-                <p className={styles.productName}>Marry Me, Mari</p>
-                <p className={styles.productPrice}>₱99,899</p>
-            </Link>
+          ))}
         </div>
-
       </div>
     </div>
   );
 };
 
-export default Shop
+export default Shop;
